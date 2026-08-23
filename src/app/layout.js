06b33afb05a5fe528a0d86b0fbddbfbd3fb08322
Priyanshu Gupta <gpriyanshu995@gmail.com>
2026-08-23 21:51:53 +0530

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,19 +3,29 @@ import TopBar from "./Components/TopBar";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
+// export const metadata = {
+//   title: "Blindfold Villa — India's First Blindfold Reality Show",
+//   description: "8 Strangers. Aankhein Band. Ek Villa. The Ultimate Test of Love.",
+//   icons: {
+//     icon: [
+//       { url: "/favicon.ico", sizes: "48x48" },
+//       { url: "/blinfoldlogochat.svg", type: "image/svg+xml" },
+//       { url: "/favicon.png", type: "image/png" },
+//     ],
+//     shortcut: "/favicon.ico",
+//     apple: "/favicon.png",
+//   },
+// };
+
+
 export const metadata = {
   title: "Blindfold Villa — India's First Blindfold Reality Show",
   description: "8 Strangers. Aankhein Band. Ek Villa. The Ultimate Test of Love.",
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "48x48" },
-      { url: "/blinfoldlogochat.svg", type: "image/svg+xml" },
-      { url: "/favicon.png", type: "image/png" },
-    ],
-    shortcut: "/favicon.ico",
-    apple: "/favicon.png",
+    icon: "/images/blindnewlogo.png"
   },
 };
+
 
 export default function RootLayout({ children }) {
   return (
